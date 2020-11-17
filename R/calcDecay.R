8f21fbe09cc9e316970be1964ff8bb8df093be22
Jan Dietrich <dietrich@pik-potsdam.de>
2020-11-17 23:26:46 +0100

    merged Decay and SteadyState calculations

--- R/calcDecay.R
+++ R/calcDecay.R
@@ -2,7 +2,9 @@
 #' @description This function wraps together the decay rate for allSOC sub-pool per year for mineral soils
 #' using the steady-state method (Tier 2) of the 2019 Refinement to the 2006 IPP Guidelines
 #' for National Greenhouse Gas Inventories
-#'
+#' @param tillage tillage type to de considered.
+#'                   Default (histill) is historic tillage area shares based on no tillage areas from Porwollik together with rule based assumption;
+#'                   'mixedtill' includes pure rule based assumptions.
 #' @return magpie object in cellular resolution
 #' @author Kristine Karstens
 #'
@@ -12,28 +14,36 @@
 #' @import madrat
 #' @import magclass
 
-calcDecay <- function() {
+calcDecay <- function(tillage="histtill") {
 
-  ActiveDecay         <- mbind(setNames(calcOutput("ActiveDecay",  landtype="crop",   aggregate = FALSE),"crop"),
-                               setNames(calcOutput("ActiveDecay",  landtype="natveg", aggregate = FALSE),"natveg"))
+  param <- readSource("IPCCSoil", convert=FALSE)
+  # kfaca = decay rate under optimum condition for active (k3)
+  # kfacs = decay rate under optimum condition for slow (k4)
+  # kfacp = decay rate under optimum condition for passive (k5)
+  # k3par1 = sand intercept
+  # k3par2 = sand slope
 
-  SlowDecay           <- mbind(setNames(calcOutput("SlowDecay",  landtype="crop",   aggregate = FALSE),"crop"),
-                               setNames(calcOutput("SlowDecay",  landtype="natveg", aggregate = FALSE),"natveg"))
+  cell.w_Factor    <- mbind(setNames(calcOutput("WaterEffectDecomposition", irrigation = "mixedirrig", aggregate = FALSE),"crop"),
+                            setNames(calcOutput("WaterEffectDecomposition", irrigation = "rainfed", aggregate = FALSE),"natveg"))
+  cell.till_Factor <- setNames(calcOutput("TillageEffectDecomposition", tillage = tillage, aggregate = FALSE)[,,rep(1,2)],c("crop","natveg"))
+  cell.till_Factor[,,"natveg"] <- calcOutput("TillageEffectDecomposition", tillage = "notill", aggregate = FALSE)[,,]
 
-  PassiveDecay        <- mbind(setNames(calcOutput("PassiveDecay", landtype="crop",   aggregate = FALSE),"crop"),
-                               setNames(calcOutput("PassiveDecay", landtype="natveg", aggregate = FALSE),"natveg"))
+  cell.t_Factor    <- calcOutput("TempEffectDecomposition", aggregate = FALSE)
+  cell.sand_frac   <- calcOutput("SandFrac", aggregate = FALSE)
 
-  subpools            <- c("active","slow","passive")
-  names               <- as.vector(outer(getNames(ActiveDecay), subpools, paste, sep="."))
-  Decay               <- new.magpie(getCells(ActiveDecay), getYears(ActiveDecay), names, fill = 0)
+  ActiveDecay    <-  param[,,"kfaca"] * cell.w_Factor * cell.t_Factor * cell.till_Factor *
+                      ( param[,,"k3par1"] +  param[,,"k3par2"] * cell.sand_frac )
+  SlowDecay      <- param[,,"kfacs"] * cell.w_Factor * cell.t_Factor * cell.till_Factor
+  PassiveDecay   <- param[,,"kfacp"] * cell.w_Factor * cell.t_Factor
 
-  Decay[,,"active"]   <- ActiveDecay
-  Decay[,,"slow"]     <- SlowDecay
-  Decay[,,"passive"]  <- PassiveDecay
+  .clean <- function(x,name) return(add_dimension(collapseNames(x),dim=3.2,add="subpool",nm=name))
 
+  decay <- magpiesort(mbind(.clean(ActiveDecay,"active"),
+                            .clean(SlowDecay,"slow"),
+                            .clean(PassiveDecay,"passive")))
 
   return(list(
-    x=Decay,
+    x=decay,
     weight=NULL,
     unit="per yr",
     description="Decay rate for all SOC sub-pool per year",
--- R/calcSteadyState.R
+++ R/calcSteadyState.R
@@ -2,7 +2,9 @@
 #' @description This function wraps together the steady state for all sub-pool SOC stock for mineral soils
 #' using the steady-state method (Tier 2) of the 2019 Refinement to the 2006 IPP Guidelines
 #' for National Greenhouse Gas Inventories
-#'
+#' @param tillage tillage type to de considered.
+#'                   Default (histill) is historic tillage area shares based on no tillage areas from Porwollik together with rule based assumption;
+#'                   'mixedtill' includes pure rule based assumptions.
 #' @return magpie object in cellular resolution
 #' @author Kristine Karstens
 #'
@@ -12,24 +14,88 @@
 #' @import madrat
 #' @import magclass
 
-calcSteadyState <- function() {
+calcSteadyState <- function(tillage="histtill") {
 
-  ActiveSteadyState  <- mbind(setNames(calcOutput("ActiveSteadyState",  landtype="crop",   aggregate = FALSE),"crop"),
-                              setNames(calcOutput("ActiveSteadyState",  landtype="natveg", aggregate = FALSE),"natveg"))
+  .steadystate <- function(alpha,decay,name) {
+    x <- toolConditionalReplace(alpha/decay[,,name], "is.na()", 0)
+    return(add_dimension(collapseNames(x),dim=3.2,add="subpool",nm=name))
+  }
 
-  SlowSteadyState    <- mbind(setNames(calcOutput("SlowSteadyState",  landtype="crop",   aggregate = FALSE),"crop"),
-                              setNames(calcOutput("SlowSteadyState",  landtype="natveg", aggregate = FALSE),"natveg"))
+  # Load fractional transfer coefficients and more parameters
+  param <- readSource("IPCCSoil", convert=FALSE)
+  # f1 = stabilization efficiencies for metabolic decay products entering the active pool
+  # f3 = stabilization efficiencies for structural decay products entering the slow pool
+  # f5 = stabilization efficiencies for active pool decay products entering the passive pool
+  # f6 = stabilization efficiencies for slow pool decay products entering the passive pool
+  # f7 = stabilization efficiencies for slow pool decay products entering the active pool
+  # f8 = stabilization efficiencies for passive pool decay products entering the active pool
 
-  PassiveSteadyState <- mbind(setNames(calcOutput("PassiveSteadyState",  landtype="crop",   aggregate = FALSE),"crop"),
-                              setNames(calcOutput("PassiveSteadyState",  landtype="natveg", aggregate = FALSE),"natveg"))
+  cell.f4_a2s      <- calcOutput("TransferActive2Slow", aggregate = FALSE)
 
-  subpools              <- c("active","slow","passive")
-  names                 <- as.vector(outer(getNames(ActiveSteadyState), subpools, paste, sep="."))
-  SoilCarbonSteadyState <- new.magpie(getCells(ActiveSteadyState), getYears(ActiveSteadyState), names, fill = 0)
+  f2_struc2a <- function(param, tillage) {
+    tillage2param       <- c(fulltill    = "f2_ft",
+                             reducedtill = "f2_rt",
+                             notill      = "f2_nt")
+    tillage2area        <- c(mixedtill    = "ruleBased",
+                             histtill     = "historicNoTill")
 
-  SoilCarbonSteadyState[,,"active"]  <- ActiveSteadyState
-  SoilCarbonSteadyState[,,"slow"]    <- SlowSteadyState
-  SoilCarbonSteadyState[,,"passive"] <- PassiveSteadyState
+    f2                  <- setNames(param[,,tillage2param], names(tillage2param))
+
+    cell.till_areaShr   <- calcOutput("TillageArea", tillage=tillage2area[tillage], aggregate = FALSE)
+    param.f2_struc2a.crop    <- dimSums(f2*cell.till_areaShr, dim=3) # stabilization efficiencies for structural decay products entering the active pool if tillage is not known
+    param.f2_struc2a.crop[param.f2_struc2a.crop==0] <- param[,,"f2_ft"]   # backup: set all cell with no area info to full tillage (just in case it is needed)
+    param.f2_struc2a.crop <- setNames(param.f2_struc2a.crop,"crop")
+    param.f2_struc2a.natveg <- param.f2_struc2a.crop
+    param.f2_struc2a.natveg[,,] <- param[,,"f2_nt"] # stabilization efficiencies for structural decay products entering the active pool if tillage is not known
+    param.f2_struc2a.natveg <- setNames(param.f2_struc2a.natveg,"natveg")
+    return(mbind(param.f2_struc2a.crop,param.f2_struc2a.natveg))
+  }
+  param.f2_struc2a   <- f2_struc2a(param,tillage)
+
+  cell.input <- calcOutput("CarbonInput", aggregate = FALSE)
+  decay      <- calcOutput("Decay", tillage=tillage, aggregate = FALSE)
+
+  ### ActiveAlpha calculations ###
+  ## Calculate all parts of carbon inflows to active pool
+  # metabolic dead organic matter transferred to active SOC sub-pool
+  cell.metabDOC_in <- cell.input[,,"metabDOC"] * param[,,"f1"]
+  # structural dead organic matter transferred to active SOC sub-pool
+  cell.strucDOC_in <- cell.input[,,"strucDOC"] * param.f2_struc2a
+  # lignin carbon in slow and passive SOC sub-pool transferred back to active SOC sub-pool
+  cell.lign_reflow <- cell.input[,,"ligninC"]  * param[,,"f3"] * (param[,,"f7"] + param[,,"f6"]*param[,,"f8"])
+  # 1 - fraction of re-transferred SOC to active SOC sub-pool
+  cell.frac_reflow <- 1 - cell.f4_a2s  * param[,,"f7"] -
+                          param[,,"f5"] * param[,,"f8"] -
+                          cell.f4_a2s  * param[,,"f6"] * param[,,"f8"]
+
+  # Bring all carbon input to the active SOC sub-pool together
+  ActiveAlpha <- dimSums(collapseNames((cell.metabDOC_in + cell.strucDOC_in + cell.lign_reflow) / cell.frac_reflow), dim=3.1)
+  ActiveSteadyState <- .steadystate(ActiveAlpha,  decay, "active")
+
+  ### SlowAlpha calculations ###
+  ## Calculate all parts of carbon inflows
+  # lignin carbon from carbon inputs transferred to slow SOC sub pool
+  cell.lign_in   <- cell.input[,,"ligninC"] * param[,,"f3"]
+  # SOC transferred from active to slow SOC sub-pool
+  cell.aSOC_in   <- ActiveSteadyState * decay[,,"active"] * cell.f4_a2s
+  # Bring all carbon input to the slow SOC sub-pool together
+  SlowAlpha <- dimSums(collapseNames(cell.lign_in + cell.aSOC_in), dim=3.1)
+  SlowSteadyState <- .steadystate(SlowAlpha,  decay, "slow")
+
+  ### PassiveAlpha calculations ###
+  ## Calculate all parts of carbon inflows
+  # SOC transferred from active to passive SOC sub-pool
+  cell.aSOC_in2   <- ActiveSteadyState * decay[,,"active"] * param[,,"f5"]
+  # SOC transferred from slow to passive SOC sub-pool
+  cell.sSOC_in    <- SlowSteadyState * decay[,,"slow"] * param[,,"f6"]
+  # Bring all carbon input to the slow SOC sub-pool together
+  PassiveAlpha     <- dimSums(collapseNames(cell.aSOC_in2 + cell.sSOC_in), dim=3.1)
+  PassiveSteadyState <- .steadystate(PassiveAlpha,  decay, "passive")
+
+
+  SoilCarbonSteadyState <-magpiesort(mbind(ActiveSteadyState,
+                                           SlowSteadyState,
+                                           PassiveSteadyState))
 
   return(list(
     x=SoilCarbonSteadyState,
