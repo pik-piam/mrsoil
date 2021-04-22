e9555e9ec77a3335a43d3cc073e0d1baacba66bc
Kristine Karstens <karstens@pik-potsdam.de>
2021-04-22 11:39:38 +0200

    remove landuse_scen handover to toolLanduseChange (not needed)

--- R/toolLanduseChange.R
+++ R/toolLanduseChange.R
@@ -1,12 +1,11 @@
 #' @title toolLanduseChange
 #' @description Calculates the cellular MAgPIE landuse change area based on given landuse data and scenario
 #'
-#' @param landuse_scen landuse configuration
-#' @param landuse       landuse data
+#' @param landuse landuse data
 #' @return List of magpie object with results on cellular level
 #' @author Kristine Karstens
 
-toolLanduseChange <- function(landuse, landuse_scen){
+toolLanduseChange <- function(landuse){
 
   years         <- getYears(landuse, as.integer = TRUE)
   LanduseChange <- landuse[,years[2:length(years)],] - setYears(landuse[,years[2:length(years)]-1,], years[2:length(years)])
@@ -16,12 +15,6 @@ toolLanduseChange <- function(landuse, landuse_scen){
   LanduseChange[,,"reduction"][LanduseChange[,,"reduction"]>0] <- 0
   LanduseChange[,,"reduction"]                                 <- (-1)*LanduseChange[,,"reduction"]
   LanduseChange[,,"expansion"][LanduseChange[,,"expansion"]<0] <- 0
-
-  if(grepl("freeze", landuse_scen)){
-    freeze_year <- as.integer(gsub("freeze","",landuse_scen))
-    reset_years <- getYears(LanduseChange, as.integer=TRUE) >= freeze_year
-    LanduseChange[,reset_years,] <- 0
-  }
 
   return(LanduseChange)
 }
