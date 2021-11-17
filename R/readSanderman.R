47464e6234efa647387eb85278caed3c4e853cf1
Kristine Karstens <karstens@pik-potsdam.de>
2021-11-17 10:47:42 +0100

    update readSanderman

--- R/readSanderman.R
+++ R/readSanderman.R
@@ -17,7 +17,7 @@
 readSanderman <- function() {
 
   data        <- as.data.frame(subset(read_excel("remnant native SOC database for release.xlsx", sheet = "sites"),
-                                      select = c("Latitude", "Longitude", "30 cm SOC")))
+                                      select = c("Latitude", "Longitude", "Native/Ag/Restored", "30 cm SOC")))
 
   raster2half    <- function(x) {
     return(trunc(x * 2) / 2 + sign(x) * 0.25)
@@ -26,13 +26,15 @@ readSanderman <- function() {
   data$Longitude <- raster2half(data$Longitude)
 
   data           <- data[complete.cases(data), ]
-  names(data)    <- c("lat", "lon", "soc")
+  names(data)    <- c("lat", "lon", "native", "soc")
 
   magpie_coord   <- Cell2Country()
   data           <- subset(merge(magpie_coord, data, by = c("lon", "lat")),
-                           select = c("lon", "lat", "celliso", "soc"))
-  out            <- new.magpie(data$celliso, NULL, names = names(data)[-3],
-                               fill = as.matrix(data[, c(1:2, 4)]),
+                           select = c("lon", "lat", "native", "celliso", "soc"))
+  data$native    <- 1*(data$native == "Native")
+
+  out            <- new.magpie(data$celliso, NULL, names = names(data)[-4],
+                               fill = as.matrix(data[, c(1:3, 5)]),
                                sets = c("celliso", "years", "data"))
 
   return(out)
